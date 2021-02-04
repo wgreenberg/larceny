dcd921c04cb5c11b5af6410c92c3d5820cf0d4cd
Will Greenberg <willg@eff.org>
2021-02-04 14:18:28 -0800

    add some unit tests for companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -10,6 +10,16 @@ class Company < ApplicationRecord
 
   def past_prices(n)
     prices = stock_prices.order(sim_time: :desc).limit n+1
-    return prices[1..]
+    return prices[1..].reverse
+  end
+
+  def total_buys(sim_time:)
+    total_buy_price = buy_orders.where(sim_time: sim_time).sum(:price)
+    price = stock_prices.where("sim_time <= ?", sim_time).order(:sim_time).last.price
+    return total_buy_price/price
+  end
+
+  def total_sells(sim_time:)
+    return sell_orders.where(sim_time: sim_time).sum(:quantity)
   end
 end
--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 class CompanyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "price" do
+    assert_equal companies(:a).price, stock_prices(:a_3).price
+    assert_equal companies(:b).price, stock_prices(:b_2).price
+  end
+
+  test "past_prices" do
+    assert_equal (companies(:b).past_prices 5), [stock_prices(:b_1)]
+    assert_equal (companies(:a).past_prices 5), [
+      stock_prices(:a_1),
+      stock_prices(:a_2),
+    ]
+
+    assert_equal (companies(:a).past_prices 1), [stock_prices(:a_2)]
+  end
+
+  test "total_sells" do
+    assert_equal companies(:a).total_sells(sim_time: 1), 3 + 5
+    assert_equal companies(:b).total_sells(sim_time: 1), 5
+
+    assert_equal companies(:a).total_sells(sim_time: 2), 1
+    assert_equal companies(:b).total_sells(sim_time: 2), 2 + 5
+  end
+
+  test "total_buys" do
+    assert_equal companies(:a).total_buys(sim_time: 1), 10
+    assert_equal companies(:a).total_buys(sim_time: 2), 5
+    assert_equal companies(:a).total_buys(sim_time: 3), 0
+
+    assert_equal companies(:b).total_buys(sim_time: 1), 5
+    assert_equal companies(:b).total_buys(sim_time: 2), 5
+    assert_equal companies(:b).total_buys(sim_time: 3), 0
+  end
 end
