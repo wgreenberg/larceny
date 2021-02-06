6c374abae5b3b150587afae2b01d029761351b2c
Will Greenberg <willg@eff.org>
2021-02-05 18:18:48 -0800

    flesh out player model a bit

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,49 @@ class Player < ApplicationRecord
   has_many :buy_orders
   has_many :sell_orders
   has_many :player_assets
+  has_many :player_cashes
+
+  def buy(company:, price:, sim_time:)
+    order = buy_orders.find_or_create_by(sim_time: sim_time, company: company)
+    order.price = price
+    order.save
+  end
+
+  def sell(company:, quantity:, sim_time:)
+    order = sell_orders.find_or_create_by(sim_time: sim_time, company: company)
+    order.quantity = quantity
+    order.save
+  end
+
+  def cash
+    cash_at Simulation.current_time
+  end
+
+  def cash_at(t)
+    player_cashes.where("sim_time <= ?", t)
+      .order(:sim_time)
+      .last
+      .amount
+  end
+
+  def update_cash(amount)
+    cash = player_cashes.find_or_create_by(sim_time: Simulation.next_time)
+    cash.amount = amount
+    cash.save
+  end
+
+  def stocks_for(company)
+    asset = player_assets.where("sim_time <= ?", Simulation.current_time)
+      .where(company: company)
+      .order(:sim_time)
+      .last
+    return 0 unless asset
+    asset.quantity
+  end
+
+  def update_stocks(company, quantity)
+    asset = player_assets.find_or_create_by(company: company, sim_time: Simulation.next_time)
+    asset.quantity = quantity
+    asset.save
+  end
 end
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,7 +1,53 @@
 require "test_helper"
 
 class PlayerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def get_buys(player:, company:, sim_time:)
+    players(player).buy_orders
+      .where(company: companies(company), sim_time: 1)
+      .all.map { |buy| buy.price }
+  end
+
+  def get_sells(player:, company:, sim_time:)
+    players(player).sell_orders
+      .where(company: companies(company), sim_time: 1)
+      .all.map { |buy| buy.quantity }
+  end
+
+  test "buy" do
+    assert_equal get_buys(player: :p1, company: :a, sim_time: 1), [10]
+    players(:p1).buy(company: companies(:a), price: 20, sim_time: 1)
+    assert_equal get_buys(player: :p1, company: :a, sim_time: 1), [20]
+  end
+
+  test "sell" do
+    assert_equal get_sells(player: :p1, company: :a, sim_time: 1), [3]
+    players(:p1).sell(company: companies(:a), quantity: 10, sim_time: 1)
+    assert_equal get_sells(player: :p1, company: :a, sim_time: 1), [10]
+  end
+
+  test "cash" do
+    assert_equal players(:p1).cash, 100
+    assert_equal players(:p2).cash, 200
+  end
+
+  test "update_cash" do
+    players(:p1).update_cash 200
+    assert_equal players(:p1).cash, 100
+    Simulation.advance
+    assert_equal players(:p1).cash, 200
+  end
+
+  test "get_assets" do
+    assert_equal players(:p1).stocks_for(companies(:a)), 10
+    assert_equal players(:p1).stocks_for(companies(:b)), 0
+    assert_equal players(:p2).stocks_for(companies(:a)), 0
+    assert_equal players(:p2).stocks_for(companies(:b)), 20
+  end
+
+  test "update_stocks" do
+    players(:p1).update_stocks(companies(:a), 20)
+    assert_equal players(:p1).stocks_for(companies(:a)), 10
+    Simulation.advance
+    assert_equal players(:p1).stocks_for(companies(:a)), 20
+  end
 end
